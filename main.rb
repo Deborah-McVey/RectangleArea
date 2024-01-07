ae09350c15181e329765c62044d198436bf8bd66
Deborah-McVey <139592532+Deborah-McVey@users.noreply.github.com>
2024-01-07 19:06:55 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,5 @@
+puts "Enter the width of the rectangle:"
+width = gets.chomp.to_i
+puts "Enter the height of the rectangle:"
+height = gets.chomp.to_i
+puts "The rectangle area is #{ width * height }."
